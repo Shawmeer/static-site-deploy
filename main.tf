7117eb40086c5225ea10d54abd42eb9742167469
shawmeer <snasri898@gmail.com>
2025-06-15 15:49:48 +0545

    fjnan

--- main.tf
+++ main.tf
@@ -90,14 +90,3 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.react_app_bucket.bucket
-}
-
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.cdn.id
-}
-
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
